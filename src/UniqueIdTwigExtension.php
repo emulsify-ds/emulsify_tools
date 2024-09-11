934b5281cfe6f0a9ef283e6b94c4a821e945b25d
Marc Berger <marc@fourkitchens.com>
2024-09-10 18:00:04 -0700

    feat(EMULSIF-94): Add unique ID function

---
+++ src/UniqueIdTwigExtension.php
@@ -0,0 +1,55 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\emulsify_tools;
+
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFunction;
+
+/**
+ * Twig extension.
+ */
+final class UniqueIdTwigExtension extends AbstractExtension {
+
+  /**
+   * A set of unique IDs that have already been used on the same page.
+   *
+   * @var int[]
+   */
+  public $uniqueIds = [];
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getFunctions(): array {
+    return [
+      new TwigFunction('uniqueId', [
+        $this,
+        'getUniqueId',
+      ]),
+    ];
+  }
+
+  /**
+   * Get a unique ID and make sure it is unique from others on the same page.
+   *
+   * @var string $prepend
+   *   An optional prepended string.
+   *
+   * @return string|int
+   *   The string combined with a random ID.
+   */
+  public function getUniqueId($prepend = NULL): string|int {
+    $id = rand(1, 1000000000);
+    while (\in_array($id, $this->uniqueIds, TRUE)) {
+      $id = rand(1, 1000000000);
+    }
+    $this->uniqueIds[] = $id;
+    $return = ($prepend) ? "$prepend-$id" : $id;
+
+    return $return;
+
+  }
+
+}
