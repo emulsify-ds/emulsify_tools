2d685e45a1cc24b97191378c0a379071ea9b5609
Callin Mullaney <57088-callinmullaney@users.noreply.drupalcode.org>
2026-06-13 02:23:24 -0500

    chore: align semantic release metadata

--- release.config.cjs
+++ release.config.cjs
@@ -1,5 +1,11 @@
 // release.config.cjs
+const parserOpts = {
+  breakingHeaderPattern: /^(\w+)(?:\(([^)]*)\))?!: (.+)$/,
+  noteKeywords: ['BREAKING CHANGE', 'BREAKING CHANGES', 'BREAKING']
+}
+
 module.exports = {
+  tagFormat: '${version}',
   branches: ['main'],
   repositoryUrl: 'https://github.com/emulsify-ds/emulsify_tools.git',
   plugins: [
@@ -7,18 +13,14 @@ module.exports = {
       '@semantic-release/commit-analyzer',
       {
         preset: 'angular',
-        parserOpts: {
-          noteKeywords: ['BREAKING CHANGE', 'BREAKING CHANGES', 'BREAKING']
-        }
+        parserOpts
       }
     ],
     [
       '@semantic-release/release-notes-generator',
       {
         preset: 'angular',
-        parserOpts: {
-          noteKeywords: ['BREAKING CHANGE', 'BREAKING CHANGES', 'BREAKING']
-        },
+        parserOpts,
         writerOpts: {
           commitsSort: ['subject', 'scope']
         }
